69a47c5dd8e1f9d6080c4bb8b6e4f1a06ca99d47
Jonathan Yeh <johnnyyeh0723@gmail.com>
2020-10-04 00:36:09 +0800

    Update convert_ssd_mobile.sh

--- convert_ssd_mobile.sh
+++ convert_ssd_mobile.sh
@@ -1,1 +1,6 @@
-python3 /opt/intel/openvino_2020.3.194/deployment_tools/model_optimizer/mo.py --input_model frozen_inference_graph.pb --tensorflow_use_custom_operations_config /opt/intel/openvino_2020.3.194/deployment_tools/model_optimizer/extensions/front/tf/ssd_v2_support.json --tensorflow_object_detection_api_pipeline_config pipeline.config --reverse_input_channels -o ../model/ssd_mobilenet_v2
+#!/bin/bash
+python3 /opt/intel/openvino_2020.3.194/deployment_tools/model_optimizer/mo.py \
+        --input_model frozen_inference_graph.pb \
+        --tensorflow_use_custom_operations_config /opt/intel/openvino_2020.3.194/deployment_tools/model_optimizer/extensions/front/tf/ssd_v2_support.json \
+        --tensorflow_object_detection_api_pipeline_config pipeline.config --reverse_input_channels \
+        -o ../model/ssd_mobilenet_v2
